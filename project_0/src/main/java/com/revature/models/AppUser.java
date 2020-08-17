236de4ec44a3ff077da9d1b9bf77d8df9c9f0f62
capolimia <capolimia@gmail.com>
2020-08-17 17:50:55 -0400

    inital commit plus base classess and User pojo

---
+++ project_0/src/main/java/com/revature/models/AppUser.java
@@ -0,0 +1,51 @@
+package com.revature.models;
+
+public class AppUser {
+    private String username;
+    private String password;
+    private Double balance;
+
+    public AppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
